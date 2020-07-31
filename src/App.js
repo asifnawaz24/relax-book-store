1801d70ed87588e21a9bf0925473b29dce43ead6
Asif Nawaz <asif_nawaz96@live.com>
2020-07-31 05:22:55 +0500

    Initial Release

--- src/App.js
+++ src/App.js
@@ -1,26 +1,384 @@
 import React from 'react';
-import logo from './logo.svg';
+import useWebAnimations from '@wellyshen/use-web-animations';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+    const sunlightKeyframes = {
+        color: ['white', 'whitesmoke', 'yellow', 'gold']
+    };
+    
+    const sunlightTiming = {
+        duration: 1000,
+        iterations: Infinity,
+        easing: 'ease-in-out',
+        direction: 'alternate'
+    };
+    
+    const { ref: sunlightChar1Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming}
+    });
+    
+    const { ref: sunlightChar2Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 100}
+    });
+    
+    const { ref: sunlightChar3Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 200}
+    });
+    
+    const { ref: sunlightChar4Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 300}
+    });
+    
+    const { ref: sunlightChar5Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 400}
+    });
+    
+    const { ref: sunlightChar6Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 500}
+    });
+    
+    const { ref: sunlightChar7Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 600}
+    });
+    
+    const { ref: sunlightChar8Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 700}
+    });
+    
+    const { ref: sunlightChar9Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 800}
+    });
+    
+    const { ref: sunlightChar10Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 900}
+    });
 
-export default App;
+    const { ref: sunlightChar11Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming}
+    });
+    
+    const { ref: sunlightChar12Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 100}
+    });
+    
+    const { ref: sunlightChar13Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 200}
+    });
+    
+    const { ref: sunlightChar14Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 300}
+    });
+    
+    const { ref: sunlightChar15Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 400}
+    });
+    
+    const { ref: sunlightChar16Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 500}
+    });
+    
+    const { ref: sunlightChar17Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 600}
+    });
+    
+    const { ref: sunlightChar18Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 700}
+    });
+    
+    const { ref: sunlightChar19Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 800}
+    });
+    
+    const { ref: sunlightChar20Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 900}
+    });
+
+    const { ref: sunlightChar21Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming}
+    });
+    
+    const { ref: sunlightChar22Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 100}
+    });
+    
+    const { ref: sunlightChar23Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 200}
+    });
+    
+    const { ref: sunlightChar24Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 300}
+    });
+    
+    const { ref: sunlightChar25Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 400}
+    });
+    
+    const { ref: sunlightChar26Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 500}
+    });
+    
+    const { ref: sunlightChar27Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 600}
+    });
+    
+    const { ref: sunlightChar28Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 700}
+    });
+    
+    const { ref: sunlightChar29Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 800}
+    });
+    
+    const { ref: sunlightChar30Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 900}
+    });
+    
+    const { ref: sunlightChar31Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming}
+    });
+    
+    const { ref: sunlightChar32Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 100}
+    });
+    
+    const { ref: sunlightChar33Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 200}
+    });
+    
+    const { ref: sunlightChar34Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 300}
+    });
+    
+    const { ref: sunlightChar35Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 400}
+    });
+    
+    const { ref: sunlightChar36Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 500}
+    });
+    
+    const { ref: sunlightChar37Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 600}
+    });
+    
+    const { ref: sunlightChar38Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 700}
+    });
+    
+    const { ref: sunlightChar39Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 800}
+    });
+    
+    const { ref: sunlightChar40Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 900}
+    });
+
+    const { ref: sunlightChar41Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming}
+    });
+    
+    const { ref: sunlightChar42Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 100}
+    });
+    
+    const { ref: sunlightChar43Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 200}
+    });
+    
+    const { ref: sunlightChar44Ref } = useWebAnimations({
+        keyframes: sunlightKeyframes,
+        timing: {...sunlightTiming, delay: 300}
+    });
+
+    const { ref: rowSecondBackgroundRef } = useWebAnimations({
+        keyframes: {
+            backgroundPosition: ['left top', 'right bottom']
+        },
+        timing: {
+            duration: 5000,
+            iterations: Infinity,
+            easing: 'ease-in-out',
+            direction: 'alternate'
+        }
+    })
+    
+    return (
+        <div>
+            <div className='topbar-desktop'>
+                <div className='topbar-sticky'>
+                <a className='menu-button' href='#front' alt=' '>Home</a>
+                <a className='menu-button' href='#products' alt=' '>Products</a>
+                <a className='menu-button' href='#about' alt=' '>About</a>
+                </div>
+            </div>
+            <div className='body' ref={rowSecondBackgroundRef}>
+                <div className='row row-first' id='front'>
+                    <div className='heading'>
+                        <span ref={sunlightChar1Ref}>R</span>
+                        <span ref={sunlightChar2Ref}>e</span>
+                        <span ref={sunlightChar3Ref}>l</span>
+                        <span ref={sunlightChar4Ref}>a</span>
+                        <span ref={sunlightChar5Ref}>x</span>
+                        <span> </span>
+                        <span ref={sunlightChar6Ref}>B</span>
+                        <span ref={sunlightChar7Ref}>o</span>
+                        <span ref={sunlightChar8Ref}>o</span>
+                        <span ref={sunlightChar9Ref}>k</span>
+                        <span> </span>
+                        <span ref={sunlightChar10Ref}>S</span>
+                        <span ref={sunlightChar11Ref}>t</span>
+                        <span ref={sunlightChar12Ref}>o</span>
+                        <span ref={sunlightChar13Ref}>r</span>
+                        <span ref={sunlightChar14Ref}>e</span>
+
+                    </div>
+                    <div className='description'>
+                        <span ref={sunlightChar15Ref}>W</span>
+                        <span ref={sunlightChar16Ref}>h</span>
+                        <span ref={sunlightChar17Ref}>e</span>
+                        <span ref={sunlightChar18Ref}>n</span>
+                        <span> </span>
+                        <span ref={sunlightChar19Ref}>d</span>
+                        <span ref={sunlightChar20Ref}>a</span>
+                        <span ref={sunlightChar21Ref}>y</span>
+                        <span> </span>
+                        <span ref={sunlightChar22Ref}>g</span>
+                        <span ref={sunlightChar23Ref}>e</span>
+                        <span ref={sunlightChar24Ref}>t</span>
+                        <span ref={sunlightChar25Ref}>s</span>
+                        <span> </span>
+                        <span ref={sunlightChar26Ref}>t</span>
+                        <span ref={sunlightChar27Ref}>o</span>
+                        <span ref={sunlightChar28Ref}>u</span>
+                        <span ref={sunlightChar29Ref}>g</span>
+                        <span ref={sunlightChar30Ref}>h</span>
+                        <span ref={sunlightChar31Ref}>,</span>
+                        <span> </span>
+                        <span ref={sunlightChar32Ref}>y</span>
+                        <span ref={sunlightChar33Ref}>o</span>
+                        <span ref={sunlightChar34Ref}>u</span>
+                        <span> </span>
+                        <span ref={sunlightChar35Ref}>n</span>
+                        <span ref={sunlightChar36Ref}>e</span>
+                        <span ref={sunlightChar37Ref}>e</span>
+                        <span ref={sunlightChar38Ref}>d</span>
+                        <span> </span>
+                        <span ref={sunlightChar39Ref}>R</span>
+                        <span ref={sunlightChar40Ref}>e</span>
+                        <span ref={sunlightChar41Ref}>l</span>
+                        <span ref={sunlightChar42Ref}>a</span>
+                        <span ref={sunlightChar43Ref}>x</span>
+                        <span ref={sunlightChar44Ref}>.</span>
+                    </div>
+                </div>
+                <div className='row row-second' id='products' >
+                    <div className='card book'>
+                            <div>
+                                <h3>
+                                    Book
+                                </h3>
+                                <p>
+                                    A book is a medium for recording information in the form of writing or images, typically composed of many pages bound together and protected by a cover.
+                                </p>
+                            </div>
+                        </div>
+                    <div className='card audiobook'>
+                        <div>
+                            <h3>
+                                Audiobook
+                            </h3>
+                            <p>
+                                An audiobook is a recording of a book or other work being read out loud.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='card comic'>
+                        <div>
+                            <h3>
+                                Comics
+                            </h3>
+                            <p>
+                                Comics is a medium used to express narratives or other ideas through images, usually combined with text. It typically takes the form of a sequence of panels of images.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='card manga'>
+                        <div>
+                            <h3>
+                                Mangas
+                            </h3>
+                            <p>
+                            Manga are comics or graphic novels originating from Japan. Most manga conform to a style developed in Japan in the late 19th century.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='row row-thrid' id='about'>
+                    <div className='row-thrid-item map'>
+                        <iframe title='google maps' width="100%" height="100%" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+                    </div>
+                    <div className='row-thrid-item contact'>
+                        <div>
+                        <i className="fa fa-industry fa-2x black" aria-hidden="true"></i> Abc, Xyz Street, Road 123, Karachi, Pakistan
+                        </div>
+                        <div>
+                        <i className="fa fa-phone fa-2x black" aria-hidden="true"></i> +92-21-31234567
+                        </div>
+                        <div>
+                        <i className="fa fa-mobile fa-2x black" aria-hidden="true"></i> +92-300-1234567
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='footer'>
+                <div>
+                    Made by <a href='https://github.com/Kindest96' target='_blank' rel='noopener noreferrer'>Asif Nawaz</a>. Official Repo for this web can be found <a href='https://github.com/Kindest96/relax-book-store' target='_blank' rel='noopener noreferrer'>HERE</a>!
+                </div>
+            </div>
+        </div>
+    )
+}
